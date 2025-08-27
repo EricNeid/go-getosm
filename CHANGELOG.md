3b428bb0f4f5c8d1d26f2104dfea9cf88aed9e55
Eric Neidhardt <eric.neidhardt@gmail.com>
2025-08-27 10:36:23 +0200

    Added option to print query

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ All notable changes to this project will be documented in this file.
 ## Unreleased
 ### Fixed
 * Query not completely printed
+### Added
+* Paramter q to print query
 
 ## 0.3.0 - 2025-08-25
 ### Added
--- cmd/osmget/main.go
+++ cmd/osmget/main.go
@@ -25,6 +25,7 @@ var (
 	timeout      = 240
 	elementLimit = 1073741824
 	verbose      = false
+	queryOutput  = false
 )
 
 func init() {
@@ -37,6 +38,7 @@ func init() {
 	flag.IntVar(&timeout, "timeout", timeout, "timeout for connection")
 	flag.IntVar(&elementLimit, "elementLimit", elementLimit, "Element limit in osm file")
 	flag.BoolVar(&verbose, "verbose", verbose, "Verbose output")
+	flag.BoolVar(&queryOutput, "q", queryOutput, "Query output")
 
 	flag.Parse()
 }
@@ -62,8 +64,10 @@ func main() {
 
 	for i, bb := range bbs {
 		log.Printf("downloading tiles %d of %d\n", i+1, len(bbs))
-
 		query := getosm.FormatQuery(bb, timeout, elementLimit)
+		if queryOutput {
+			log.Printf("download using query %s\n", query)
+		}
 		result, err := getosm.Download(apiURL, query)
 		for retry := 1; err != nil && retry <= maxRetries; retry++ {
 			log.Printf("error downloading data: %v, attempting retry %d of %d in %s seconds\n", err, retry, maxRetries, retryDelay)
